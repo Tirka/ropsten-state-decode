9f6e832bfb44282f85fdf6da45e656c97aa82377
Maksim Vykhota <m.vykhota@protonmail.com>
2022-11-28 02:28:21 +0300

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,62 @@
-use std::fs::File;
+use clap::Parser;
 use rlp::Rlp;
 
+/// Simple program to parse Geth snapshots
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Input RLP snapshot file path
+   #[arg(short, long, value_name = "FILE")]
+   snapshot: String,
+}
+
 fn main() {
-    let state_path = "/home/maksimv/ropsten-state.rlp";
-    let state_path = "/home/maksimv/ropsten-state-stripped.rlp";
-    // let state_path = "test.rlp";
-    let state_file = File::open(state_path).unwrap();
+    let args = Args::parse();
 
-    let mmap = unsafe { memmap::MmapOptions::new().map(&state_file).unwrap() };
+    let snapshot_full_path = std::fs::canonicalize(&args.snapshot)
+        .expect("Can't get absolute file path of snapshot");
+    
+    let snapshot_file_name = snapshot_full_path.iter().last()
+        .expect("Can't parse absolute path")
+        .to_string_lossy();
 
-    println!("ropsten-state.rlp");
+    let snapshot = std::fs::File::open(&snapshot_full_path)
+        .unwrap_or_else(|_| panic!("Can't open snapshot file: '{}'", &args.snapshot));
 
-    let rlp = Rlp::new(mmap.as_ref());
+    let snapshot = unsafe { memmap::MmapOptions::new().map(&snapshot).unwrap() };
+
+    println!("File: {snapshot_file_name}");
+
+    let rlp = Rlp::new(&snapshot);
 
     proceed_rlp(0, rlp);
 }
 
 fn proceed_rlp(nest_lvl: u32, rlp: Rlp) {
-    for _ in 0..nest_lvl {
-        print!("    ");
-    }
+    print_padding(nest_lvl);
 
     match rlp.prototype().unwrap() {
         rlp::Prototype::Null => {
-            println!("Null");
+            println!("<NULL>");
         },
         rlp::Prototype::Data(len) => {
-            println!("Data({len}): {}", hex::encode(rlp.as_val::<Vec<u8>>().unwrap()));
+            let data: Vec<u8> = rlp.as_val().unwrap();
+            let hex = if data.is_empty() { "0".to_string() } else { hex::encode(&data) };
+            let as_string = std::str::from_utf8(&data).map(|s| s.trim())
+                .unwrap_or("<N/A>");
+            println!("Data({len}): 0x{} [{}]", hex, as_string);
         },
         rlp::Prototype::List(len) => {
-            println!("List({len})");
-            for (_idx, child) in rlp.into_iter().enumerate() {
+            println!("List({len}):");
+            for (_, child) in rlp.into_iter().enumerate() {
                 proceed_rlp(nest_lvl + 1, child);
             }
         },
+    }
+}
+
+fn print_padding(nest_lvl: u32) {
+    for _ in 0..nest_lvl {
+        print!("    ");
     }
 }
